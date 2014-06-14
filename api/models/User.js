1cbd357744dc1aa99f303f65439a14cb292487c5
Frank Cangialosi <fcangialosi94@gmail.com>
2014-06-14 14:48:45 -0400

    initial commit

---
+++ api/models/User.js
@@ -0,0 +1,32 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	name : {
+  		type : 'string',
+  		required : true
+  	},
+
+  	email : {
+  		type : 'string',
+  		email : true,
+  		required : true,
+  		unique : true
+  	},
+
+  	encryptedPassword: {
+  		type : 'string',
+  		required : true
+  	}
+    
+  }
+
+};
