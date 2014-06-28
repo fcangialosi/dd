9d43894a1c033fa99127d0c1bedcf1e2ca2830d3
Frank Cangialosi <fcangialosi94@gmail.com>
2014-06-28 11:04:14 -0400

    added form validation for all the other forms

--- assets/linker/js/form.js
+++ assets/linker/js/form.js
@@ -1,4 +1,4 @@
-$('.ui.form')
+$('#signup-form')
   .form({
     firstName: {
       identifier  : 'firstName',
@@ -77,6 +77,144 @@ $('.ui.form')
         type : 'match[password]',
         prompt : "Woops, you must have made a small typo! The two passwords don't match"
       }
+      ]
+    }
+  })
+;
+$('#signin-form')
+  .form({
+    email: {
+      identifier : 'email',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : "Please enter an email address"
+        },
+        {
+          type : 'email',
+          prompt : 'Please enter a valid email address'
+        }
+      ]
+    },
+    password: {
+      identifier : 'password',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a password'
+        },
+        {
+          type   : 'length[6]',
+          prompt : 'Your password must be at least 6 characters'
+        }
+      ]
+    }
+  })
+;
+$('#delivery-form')
+  .form({
+    contactName: {
+      identifier  : 'name',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter the name of who we can contact regarding your order'
+        }
+      ]
+    },
+    contactPhone : {
+      identifier : 'phone',
+      rules : [
+        {
+          type : 'empty',
+          prompt : 'Please enter a the phone number of who we can contact regarding your order'
+        },
+        {
+          type : 'phone',
+          prompt : 'Please enter a valid phone number'
+        }
+      ]
+    },
+    deliveryAddress: {
+      identifier : 'address',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a delivery address'
+        }
+      ]
+    },
+    deliveryCity: {
+      identifier : 'city',
+      rules: [
+        {
+          type : 'empty',
+          prompt : 'Please enter the city name'
+        }
+      ]
+    }
+  })
+;
+$('#card-form')
+  .form({
+    cardNumber: {
+      identifier  : 'number',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter your credit card number'
+        },
+        {
+          type   : 'maxLength[19]',
+          prompt : 'Please enter a valid credit card number'
+        },
+        {
+          type   : 'length[12]',
+          prompt : 'Please enter a valid credit card number'
+        }
+      ]
+    },
+    cardName: {
+      identifier  : 'name',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter your name as it appears on your card'
+        }
+      ]
+    },
+    cardExpiry: {
+      identifier : 'expiry',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : "Please enter the experation date of your credit card"
+        },
+        {
+          type   : 'length[9]',
+          prompt : 'Please make sure you entered a valid experation date!'
+        },
+        {
+          type   : 'maxLength[9]',
+          prompt : 'Please make sure you entered a valid experation date!'
+        }
+      ]
+    },
+    cardCvc : {
+      identifier : 'cvc',
+      rules : [
+        {
+          type : 'empty',
+          prompt : 'Please enter the 3-digit CVC code on the back of your card'
+        },
+        {
+          type   : 'length[3]',
+          prompt : 'Your CVC code should be exactly 3 numbers'
+        },
+        {
+          type   : 'maxLength[3]',
+          prompt : 'Your CVC code should be exactly 3 numbers'  
+        }
       ]
     }
   })
