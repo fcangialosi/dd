213f18fd31a64e926f8530b9bd02d5158a10fe46
Frank Cangialosi <fcangialosi94@gmail.com>
2014-08-05 17:21:56 -0400

    commented out prod variables

--- config/session.js
+++ config/session.js
@@ -1,7 +1,7 @@
 /**
  * Session
- * 
- * Sails session integration leans heavily on the great work already done by Express, but also unifies 
+ *
+ * Sails session integration leans heavily on the great work already done by Express, but also unifies
  * Socket.io with the Connect session store. It uses Connect's cookie parser to normalize configuration
  * differences between Express and Socket.io and hooks into Sails' middleware interpreter to allow you
  * to access and auto-save to `req.session` with Socket.io the same way you would with Express.
@@ -14,23 +14,23 @@ module.exports.session = {
 
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
-  // forcing them to log in again. 
+  // forcing them to log in again.
   secret: 'ff3d1c370b9212986f215bfa0a73b9ca',
 
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
-  adapter: 'redis',
+  // adapter: 'redis',
   //
   // The following values are optional, if no options are set a redis instance running
   // on localhost is expected.
   // Read more about options at: https://github.com/visionmedia/connect-redis
   //
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  // host: process.env.REDIS_HOST,
+  // port: process.env.REDIS_PORT,
   //ttl: <redis session TTL in seconds>,
-  db: process.env.REDIS_DB,
-  pass: process.env.REDIS_PASS
+  // db: process.env.REDIS_DB,
+  // pass: process.env.REDIS_PASS
   // prefix: 'sess:'
 
 
