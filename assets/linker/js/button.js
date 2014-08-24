d63ab90ac3e5fb2e03b50c44812502fe5cd91957
Frank Cangialosi <fcangialosi94@gmail.com>
2014-08-24 13:04:37 -0400

    moved time selection before location selection on delivery page

--- assets/linker/js/button.js
+++ assets/linker/js/button.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
+
+  $('#delivery-datepicker').focusout(function(){
+    date = $('#delivery-datepicker').val()
+    $('#delivery-date').attr('value', date);
+  });
+  $('#delivery-timepicker').focusout(function(){
+    time = $('#delivery-timepicker').val()
+    $('#delivery-time').attr('value', time);
+  });
+
   var $change = $('.ui.change.button');
-  var handler = {
+  var change_handler = {
     activate: function() {
       $change.removeClass('active').text("Select"); // wipe all selected
       $(this).addClass('active').text("Selected"); // select just the clicked option
       // set hidden field in form as the index of this option in the saved options array
-      $('#option-index').attr('value', $(this).attr('index')); 
+      $('#option-index').attr('value', $(this).attr('index'));
     }
   };
-  $change.on('click', handler.activate);
+  $change.on('click', change_handler.activate);
 });
