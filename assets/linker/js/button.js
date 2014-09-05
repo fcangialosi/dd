78638004d55aa4183582b75e6154c8e50cf479d5
Frank Cangialosi <fcangialosi94@gmail.com>
2014-09-05 01:51:58 -0400

    made everything responsive and mobile-ready

--- assets/linker/js/button.js
+++ assets/linker/js/button.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
   var $change = $('.ui.change.button');
   var change_handler = {
     activate: function() {
-      $change.removeClass('active').text("Select"); // wipe all selected
+      $change.removeClass('active').text("Select This Location"); // wipe all selected
       $(this).addClass('active').text("Selected"); // select just the clicked option
       // set hidden field in form as the index of this option in the saved options array
       $('#option-index').attr('value', $(this).attr('index'));
