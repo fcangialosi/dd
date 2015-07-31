9fd811f278f8ce0c24e857a809ee286455ecb92d
sbfcangialosi <fcangialosi94@gmail.com>
2015-07-31 19:38:56 -0400

    only display last four digits of card in email

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -37,10 +37,11 @@ var generateHtml = function(session, cart, rawNumber) {
   }
   var payment_obj = {};
   payment_obj['method'] = session['paymentMethod'];
-  //payment_obj['info'] = '';
-  //if (session['paymentMethod'] === 'Credit') {
-  //  payment_obj['info'] = session['card']['cardName'] + "<br>" + rawNumber + "<br>" + session['card']['cardExpiry'] + "<br>" + session['card']['cardCvc'];
-  //}
+  payment_obj['info'] = '';
+  if (session['paymentMethod'] === 'Credit') {
+    // payment_obj['info'] = session['card']['cardName'] + "<br>" + rawNumber + "<br>" + session['card']['cardExpiry'] + "<br>" + session['card']['cardCvc'];
+    payment_obj['info'] = ("XXXX-XXXX-XXXX-"+session['card']['lastFour']);
+  }
   return tpl({
     order_contact : session['User'],
     delivery : session['delivery'],
