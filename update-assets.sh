43a76a18d00d3636eca541da7537940ebe8249e2
sbfcangialosi <fcangialosi94@gmail.com>
2015-11-28 11:06:07 -0500

    fixed update script to remove old assets

--- update-assets.sh
+++ update-assets.sh
@@ -8,4 +8,6 @@ echo -n $NEW_VERSION >> $VERSION_STORE
 echo "Old version was $OLD_VERSION"
 echo "New version is $NEW_VERSION"
 
+find .tmp/public/min -type f -name production* -delete
 sed -i "s/$OLD_VERSION/$NEW_VERSION/g" Gruntfile.js
+
